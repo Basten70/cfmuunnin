0fd487eff14433c98e508e41fe458806a9bf7b50
Basten70 <90454559+Basten70@users.noreply.github.com>
2021-09-15 08:09:19 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+const fahreinheitSyote=document.querySelector('input');
+const fahrenheitTuloste=document.querySelector('#fahrenheit');
+const celsiusTuloste=document.querySelector('#celsius');
+const muunnaButton=document.querySelector('button');
+
+function fahrenheitCelsiukseksi(fahrenheit){
+    let celsius=fahrenheit;
+    return celsius;
+}
+function muunna(){
+    let syote=fahreinheitSyote.value;
+    fahrenheitTuloste.textContent=syote;
+    celsiusTuloste.textContent=fahrenheitCelsiukseksi(syote);
+}
+
+muunnaButton.addEventListener('click', muunna);
